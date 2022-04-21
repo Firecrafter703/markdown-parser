6d7fecb0a8c4c1ff1e8835c0f58f8c48814f2c42
DK703 <91508871+Firecrafter703@users.noreply.github.com>
2022-04-21 15:34:12 -0700

    Pushing everything

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,56 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.io.IOException;
+
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
         //javac -cp ".;lib\junit-4.13.2.jar;lib\hamcrest-core-1.3.jar" MarkdownParseTest.java
         //java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore MarkdownParseTest
+
+        
+        
+    }
+    @Test
+    public void getLinksTest() throws IOException
+    {
+        List somethingList = List.of("https://something.com", "some-thing.html");
+        MarkdownParse mP = new MarkdownParse();
+        Path p = Path.of("test-file.md");
+        String Content = Files.readString(p);
+        ArrayList<String> output = mP.getLinks(Content);
+        assertEquals(somethingList, output);
+
+        
+    }
+
+    @Test
+    public void getLinksTest2() throws IOException
+    {
+        List somethingList = List.of("https://www.nintendo.com", "htps/ucsd-cse11-f21.githb.io/syllabus.html//", "hp(");
+        MarkdownParse mP = new MarkdownParse();
+        Path p = Path.of("Test-file2.md");
+        String Content = Files.readString(p);
+        ArrayList<String> output = mP.getLinks(Content);
+        assertEquals(somethingList, output);
+
+        
+    }
+
+    @Test
+    public void getLinksTest3() throws IOException
+    {
+        List somethingList = List.of("fef");
+        MarkdownParse mP = new MarkdownParse();
+        Path p = Path.of("test-file3.md");
+        String Content = Files.readString(p);
+        ArrayList<String> output = mP.getLinks(Content);
+        assertEquals(somethingList, output);
+
+        
     }
 }
